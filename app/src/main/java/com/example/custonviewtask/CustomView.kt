02f8ed84ac45ec6377b3e56cc15ec09bb401ee56
Tytyanchuk Sergey <xsergey87x@ukr.net>
2023-04-04 21:48:34 +0300

    first Commit

---
+++ app/src/main/java/com/example/custonviewtask/CustomView.kt
@@ -0,0 +1,16 @@
+package com.example.custonviewtask
+
+import android.content.Context
+import android.util.AttributeSet
+import androidx.constraintlayout.widget.ConstraintLayout
+
+class CustomView(
+    context: Context,
+    attrs : AttributeSet?,
+    defStyleAttr: Int,
+    desStyleRes: Int
+) : ConstraintLayout(context, attrs, defStyleAttr) {
+
+
+
+}
