380aa8ffd48b9f120a5096f899285ff15b110372
Sergey <xsergey87x@ukr.net>
2023-04-05 09:30:31 +0300

    4th commit

--- app/src/main/java/com/example/custonviewtask/CustomView.kt
+++ app/src/main/java/com/example/custonviewtask/CustomView.kt
@@ -2,7 +2,9 @@ package com.example.custonviewtask
 
 import android.content.Context
 import android.util.AttributeSet
+import android.view.LayoutInflater
 import androidx.constraintlayout.widget.ConstraintLayout
+import com.example.custonviewtask.databinding.CustomViewBinding
 
 class CustomView(
     context: Context,
@@ -11,8 +13,18 @@ class CustomView(
     desStyleRes: Int
 ) : ConstraintLayout(context, attrs, defStyleAttr, desStyleRes) {
 
+    private val binding: CustomViewBinding
+
     constructor(context: Context, attrs: AttributeSet?, defStyleAttr: Int): this(context, attrs, defStyleAttr, 0)
     constructor(context: Context, attrs: AttributeSet?): this(context,attrs,0)
     constructor(context: Context): this(context, null)
+
+
+    init {
+        val inflater = LayoutInflater.from(context)
+        inflater.inflate(R.layout.custom_view,this,true)
+        binding = CustomViewBinding.bind(this)
+    }
+
 
 }
